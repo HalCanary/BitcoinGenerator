a663e201da302626c0f7312c2d82fc8a68e17939
Hal Canary <hal@cs.unc.edu>
2013-04-05 07:55:04 -0400

    OFFLINE_SAVINGS_WALLET; entropy from /dev/random

--- RandomWords.py
+++ RandomWords.py
@@ -6,12 +6,20 @@ Written 2013 Hal Canary.
 Dedicated to the public domain.
 '''
 import random,math,sys,os
+useDevRandom = True
+def GetRandom():
+	if useDevRandom:
+		with open('/dev/random', 'r') as f:
+			random.seed(f.read(16))
+		return random
+	else:
+		return random.SystemRandom()
+
 with open(os.path.expanduser('~/Downloads/diceware.wordlist.asc')) as f:
 	X = [line.split()[1] for nu,line in enumerate(f) if 2 <= nu < 7778]
 required_entropy = 128
 numwords = int(math.ceil(required_entropy / math.log(len(X),2)))
-s = ' '.join(random.SystemRandom().choice(X)
-			   for i in xrange(numwords))
+s = ' '.join(GetRandom().choice(X) for i in xrange(numwords))
 sys.stdout.write(s)
 sys.stdout.flush()
 sys.stderr.write('\n')
